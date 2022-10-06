0423e4f9d1dc10caf78635a6108b1a0b29afe094
liam <clark.liam@hotmail.com>
2022-10-06 17:16:49 +1030

    Finished

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,21 @@
-import { httpGet } from './mock-http-interface';
-
-// TODO define this type properly
-type TResult = any;
-
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+import { httpGet } from "./mock-http-interface";
+type TResult = {
+  [key: string] : string;
+};
+/**
+ * Call GET endpoints and return the body.message of each endpoint
+ * assumes the endpoint either returns 200 or 500
+ * @param urls array of GET endpoints
+ * @returns Promise that resolves to an array of Objects of type TResult
+ */
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+  const getQuotes = Promise.all(
+    urls.map(async (url) => {
+      const quoteRes = await httpGet(url);
+      return {
+        [quoteRes.status == 200 ? "Arnie Quote" : "FAILURE"]: JSON.parse(quoteRes.body).message,
+      };
+    })
+  );
+  return getQuotes;
 };
